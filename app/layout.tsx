48d7e3915da052295eac60171d3ecd3829d07e82
Daniel Villanueva <danvil@on.sinch.com>
2026-07-18 21:04:51 -0600

    Update page title to Jolidays - Vallarta 2026

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Jolidays - Intercambio Familiar 2026",
+  title: "Jolidays - Vallarta 2026",
   description: "Intercambio de regalos navideño - Riviera Nayarit, diciembre 2026",
 };
 
